8e88981ddba48f28c6b4d254a29140e6421bcbeb
Florent <4105569+Auze@users.noreply.github.com>
2023-07-09 19:33:52 +0200

    Reverse: Update configuration

--- nginx/config/init-letsencrypt.sh
+++ nginx/config/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(fediland.org)
+domains=(fediland.org www.fediland.org monitoring.fediland.org)
 rsa_key_size=4096
 data_path="/data/reverse-proxy/certbot"
 email="" # Adding a valid address is strongly recommended
