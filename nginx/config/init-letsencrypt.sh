94b633eb99cbce9c4abc6f08bc02d0614dfe424b
Florent <4105569+Auze@users.noreply.github.com>
2023-07-09 18:04:24 +0200

    update domain name

--- nginx/config/init-letsencrypt.sh
+++ nginx/config/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(social.glorious-space.eu)
+domains=(fediland.org)
 rsa_key_size=4096
 data_path="/data/reverse-proxy/certbot"
 email="" # Adding a valid address is strongly recommended
